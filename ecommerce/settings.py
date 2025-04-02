192c7798a726263f0bc98960172c811fc9aa2039
Mauricio <mauballe11@gmail.com>
2025-04-01 22:29:32 -0600

    twelve

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -225,4 +225,21 @@ CORS_ALLOW_METHODS = [
     "OPTIONS",
 ]
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
 
